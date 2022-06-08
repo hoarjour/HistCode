638f1f1319cca8e6df58ebda852283a09b398f09
hoarjour <3263781582@qq.com>
2022-06-08 15:26:04 +0800

    Add train clf model code

--- README.md
+++ README.md
@@ -15,28 +15,51 @@ You can download your own wsi dataset to the directory slides,
 then run data_processing/create_patches_fp.py to seg and tile wsis, 
 adjust the parameters according to your needs.  
 For example, you can use following command for segment and tile.  
-```python create_patches_fp.py --source ../slides/TCGA-LUNG --data_type tcga_lung --patch_size 256 --save_dir ../tile_results --patch --seg```  
+``` shell
+python create_patches_fp.py --source ../slides/TCGA-LUNG --data_type tcga_lung --patch_size 256 --save_dir ../tile_results --patch --seg
+```  
 When you run this command, it will run in default parameter, if you want to run with your parameter, you can modify tcga_lung.csv in directory preset, and add ```--preset ../preset/tcga_lung.csv```.
+Then the coordinate files will be saved to ```tile_results/patches``` and the mask files that show contours of slides will be saved to ```tile_results/masks```.
 
 ## Train Contrast Learning Model
-Run train/train_adco.py to train contrast learning model on tiles, 
-you should write Adco/ops/argparser.py to configure the data source 
-and the save address and ADCO related parameters firstly. 
-In addition, you need to prepare a CSV file similar to dataset_csv/sample_data.csv, 
+Run train/train_adco.py to train contrast learning model on tiles,
+you should write Adco/ops/argparser.py to configure the data source
+and the save address and ADCO related parameters firstly.
+In addition, you need to prepare a CSV file similar to dataset_csv/sample_data.csv,
 this file needs to save the name of the WSI file used for training.  
 For example, you can use following command for training ADCO model with default parameter.  
-```python train_adco.py --csv_path ../dataset_csv/sample_data.csv --save_path ../MODELS --data_h5_dir ../tile_result --data_slide_dir ../slides/TCGA-LUNG --data_type tcga_lung```
+``` shell
+python train_adco.py --csv_path ../dataset_csv/sample_data.csv --save_path ../MODELS --data_h5_dir ../tile_result --data_slide_dir ../slides/TCGA-LUNG --data_type tcga_lung
+```  
 
-## extract tile-level features
+
+## Extract Tile-Level Features
 Run data_processing/extract_features_fp.py to extract the tile-level features.
-You should add the following parameters When running.
-- data_h5_dir: directory for storing tile results, default tile_results.
-- data_slide_dir: directory for storing WSIs, default slides/TCGA-LUNG.
-- csv_path: the csv file that saves the name of the WSI files used for training, like dataset_csv/sample_data.csv.
-- feat_dir: directory for storing tile-level features, default FEATURES.
-- model_type: the model you used for extract tile-level features, default ADCO.
-- data_type: data source name, default tcga_lung.
-- model_path: weight path, default models/cl_10_tcga_lung_not_sym_200.pth.tar
 For example, you can use following command for extracting features.  
-```python extract_features_fp.py --data_h5_dir ../tile_results --data_slide_dir ../slides/TCGA-LUNG --csv_path ../dataset_csv/sample_data.csv --feat_dir ../FEATURES --data_type tcga_lung --model_path ../MODELS/adco_tcga_lung_not_sym.pth.tar```
+``` shell
+python extract_features_fp.py --data_h5_dir ../tile_results --data_slide_dir ../slides/TCGA-LUNG --csv_path ../dataset_csv/sample_data.csv --feat_dir ../FEATURES --data_type tcga_lung --model_path ../MODELS/adco_tcga_lung_not_sym.pth.tar
+```  
+The above command will use the trained ADCO model in ```model_path``` to extract tile features in ```data_slide_dir```
+and save the features to ```feat_dir```. 
 
+## Train Classification Model
+Run train/train_clf_model.py to perform downstream classification task. For example:  
+``` shell
+python train_clf_model.py --data_root_dir ../FEATURES --extract_model ADCO --results_dir ../results
+```  
+The above command will use the feature file in ```data_root_dir``` to train the classification model, and then output the test results to ```results_dir```.
+User needs to divide the data set into training set, verification set and test set in advance and put them under dataset_csv/clf, such as:  
+``` bash
+dataset_csv/clf
+	     ├── train_dataset_1.csv
+	     ├── ...
+	     ├── train_dataset_5.csv
+	     ├── test_dataset_1.csv
+	     ├── ...
+	     ├── test_dataset_5.csv
+	     ├── val_dataset_1.csv
+	     ├── ...
+	     ├── val_dataset_5.csv
+```  
+The default number of folds is 5, if user want to change fold numbers, add ```--k fold_number``` and prepare corresponding training files in dataset_csv/clf.
+The training files is like dataset_csv/clf/sample_data2.csv.
---
+++ utils/core_utils.py
@@ -0,0 +1,272 @@
+from utils.utils import *
+import os
+from networks.model_mil import MIL_fc, MIL_fc_mc
+from networks.model_clam import CLAM_MB, CLAM_SB
+from sklearn.preprocessing import label_binarize
+from sklearn.metrics import roc_auc_score, roc_curve
+from sklearn.metrics import auc as calc_auc
+
+
+class Accuracy_Logger(object):
+    """Accuracy logger"""
+    def __init__(self, n_classes):
+        super(Accuracy_Logger, self).__init__()
+        self.n_classes = n_classes
+        self.initialize()
+
+    def initialize(self):
+        self.data = [{"count": 0, "correct": 0} for i in range(self.n_classes)]
+    
+    def log(self, Y_hat, Y):
+        Y_hat = int(Y_hat)
+        Y = int(Y)
+        self.data[Y]["count"] += 1
+        self.data[Y]["correct"] += (Y_hat == Y)
+    
+    def log_batch(self, Y_hat, Y):
+        Y_hat = np.array(Y_hat).astype(int)
+        Y = np.array(Y).astype(int)
+        for label_class in np.unique(Y):
+            cls_mask = Y == label_class
+            self.data[label_class]["count"] += cls_mask.sum()
+            self.data[label_class]["correct"] += (Y_hat[cls_mask] == Y[cls_mask]).sum()
+    
+    def get_summary(self, c):
+        count = self.data[c]["count"] 
+        correct = self.data[c]["correct"]
+        
+        if count == 0: 
+            acc = None
+        else:
+            acc = float(correct) / count
+        
+        return acc, correct, count
+
+
+def train(datasets, cur, device, args):
+    """   
+        train for a single fold
+    """
+    print('\nTraining Fold {}!'.format(cur))
+    writer_dir = os.path.join(args.results_dir, str(cur))
+    if not os.path.isdir(writer_dir):
+        os.mkdir(writer_dir)
+
+    if args.log_data:
+        from tensorboardX import SummaryWriter
+        writer = SummaryWriter(writer_dir, flush_secs=15)
+
+    else:
+        writer = None
+
+    print('\nInit train/val/test splits...', end=' ')
+    train_split, val_split, test_split = datasets
+    print('Done!')
+    print("Training on {} samples".format(len(train_split)))
+    print("Validating on {} samples".format(len(val_split)))
+    print("Testing on {} samples".format(len(test_split)))
+
+    print('\nInit loss function...', end=' ')
+    if args.bag_loss == 'svm':
+        from topk.svm import SmoothTop1SVM
+        loss_fn = SmoothTop1SVM(n_classes = args.n_classes)
+        if device.type == 'cuda':
+            loss_fn = loss_fn.cuda()
+    else:
+        loss_fn = nn.CrossEntropyLoss().to(device)
+    print('Done!')
+    
+    print('\nInit Model...', end=' ')
+    model_dict = {"dropout": args.drop_out, 'n_classes': args.n_classes}
+    if args.model_type == 'clam' and args.subtyping:
+        model_dict.update({'subtyping': True})
+
+    if args.model_size is not None and args.model_type != 'mil':
+        model_dict.update({"size_arg": args.model_size})
+
+    if args.model_type in ['clam_sb', 'clam_mb']:
+        if args.subtyping:
+            model_dict.update({'subtyping': True})
+
+        if args.B > 0:
+            model_dict.update({'k_sample': args.B})
+
+        if args.inst_loss == 'svm':
+            from topk.svm import SmoothTop1SVM
+            instance_loss_fn = SmoothTop1SVM(n_classes = 2)
+            if device.type == 'cuda':
+                instance_loss_fn = instance_loss_fn.cuda()
+        else:
+            instance_loss_fn = nn.CrossEntropyLoss().to(device)
+
+        if args.model_type =='clam_sb':
+            model = CLAM_SB(**model_dict, instance_loss_fn=instance_loss_fn)
+        elif args.model_type == 'clam_mb':
+            model = CLAM_MB(**model_dict, instance_loss_fn=instance_loss_fn)
+        else:
+            raise NotImplementedError
+
+    else: # args.model_type == 'mil'
+        if args.n_classes > 2:
+            model = MIL_fc_mc(**model_dict)
+        else:
+            model = MIL_fc(**model_dict)
+    model.relocate(device)
+
+    print('Done!')
+
+    print('\nInit optimizer ...', end=' ')
+    optimizer = get_optim(model, args)
+    print('Done!')
+    
+    print('\nInit Loaders...', end=' ')
+    train_loader = get_split_loader(train_split, training=True, testing = args.testing, weighted = args.weighted_sample)
+    val_loader = get_split_loader(val_split,  testing = args.testing)
+    test_loader = get_split_loader(test_split, testing = args.testing)
+    print('Done!')
+
+    for epoch in range(args.max_epochs):
+        train_loop_clam(epoch, model, device, train_loader, optimizer, args.n_classes, args.bag_weight, writer, loss_fn)
+
+    torch.save(model.state_dict(), os.path.join(args.results_dir, "s_{}_{}_checkpoint.pt".format(cur, args.extract_model)))
+
+    _, val_error, val_auc, _= summary(model, val_loader, args.n_classes)
+    print('Val error: {:.4f}, ROC AUC: {:.4f}'.format(val_error, val_auc))
+
+    results_dict, test_error, test_auc, acc_logger = summary(model, test_loader, args.n_classes)
+    print('Test error: {:.4f}, ROC AUC: {:.4f}'.format(test_error, test_auc))
+
+    for i in range(args.n_classes):
+        acc, correct, count = acc_logger.get_summary(i)
+        print('class {}: acc {}, correct {}/{}'.format(i, acc, correct, count))
+
+        if writer:
+            writer.add_scalar('final/test_class_{}_acc'.format(i), acc, 0)
+
+    if writer:
+        writer.add_scalar('final/val_error', val_error, 0)
+        writer.add_scalar('final/val_auc', val_auc, 0)
+        writer.add_scalar('final/test_error', test_error, 0)
+        writer.add_scalar('final/test_auc', test_auc, 0)
+        writer.close()
+    return results_dict, test_auc, 1-test_error, val_auc, 1-val_error
+
+
+def train_loop_clam(epoch, model, device, loader, optimizer, n_classes, bag_weight, writer = None, loss_fn = None):
+    print(device)
+    model.train()
+    acc_logger = Accuracy_Logger(n_classes=n_classes)
+    inst_logger = Accuracy_Logger(n_classes=n_classes)
+    
+    train_loss = 0.
+    train_error = 0.
+    train_inst_loss = 0.
+    inst_count = 0
+
+    print('\n')
+
+    for batch_idx, (data, label) in enumerate(loader):
+        data, label = data.to(device), label.to(device)
+
+        logits, Y_prob, Y_hat, _, instance_dict = model(data, label=label, instance_eval=True)
+
+        acc_logger.log(Y_hat, label)
+        loss = loss_fn(logits, label)
+        loss_value = loss.item()
+
+        instance_loss = instance_dict['instance_loss']
+        inst_count += 1
+        instance_loss_value = instance_loss.item()
+        train_inst_loss += instance_loss_value
+        
+        total_loss = bag_weight * loss + (1-bag_weight) * instance_loss
+
+        inst_preds = instance_dict['inst_preds']
+        inst_labels = instance_dict['inst_labels']
+        inst_logger.log_batch(inst_preds, inst_labels)
+
+        train_loss += loss_value
+        if (batch_idx + 1) % 20 == 0:
+            print('batch {}, loss: {:.4f}, instance_loss: {:.4f}, weighted_loss: {:.4f}, '.format(batch_idx, loss_value, instance_loss_value, total_loss.item()) +
+                  'label: {}, bag_size: {}'.format(label.item(), data.size(0)))
+
+        error = calculate_error(Y_hat, label)
+        train_error += error
+
+        # backward pass
+        total_loss.backward()
+        # step
+        optimizer.step()
+        optimizer.zero_grad()
+
+    # calculate loss and error for epoch
+    train_loss /= len(loader)
+    train_error /= len(loader)
+    
+    if inst_count > 0:
+        train_inst_loss /= inst_count
+        print('\n')
+        for i in range(2):
+            acc, correct, count = inst_logger.get_summary(i)
+            print('class {} clustering acc {}: correct {}/{}'.format(i, acc, correct, count))
+
+    print('Epoch: {}, train_loss: {:.4f}, train_clustering_loss:  {:.4f}, train_error: {:.4f}'.format(epoch, train_loss, train_inst_loss,  train_error))
+    for i in range(n_classes):
+        acc, correct, count = acc_logger.get_summary(i)
+        print('class {}: acc {}, correct {}/{}'.format(i, acc, correct, count))
+        if writer and acc is not None:
+            writer.add_scalar('train/class_{}_acc'.format(i), acc, epoch)
+
+    if writer:
+        writer.add_scalar('train/loss', train_loss, epoch)
+        writer.add_scalar('train/error', train_error, epoch)
+        writer.add_scalar('train/clustering_loss', train_inst_loss, epoch)
+
+
+def summary(model, loader, n_classes):
+    device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    acc_logger = Accuracy_Logger(n_classes=n_classes)
+    model.eval()
+    test_loss = 0.
+    test_error = 0.
+
+    all_probs = np.zeros((len(loader), n_classes))
+    all_labels = np.zeros(len(loader))
+
+    slide_ids = loader.dataset.slide_data['slide_id']
+    patient_results = {}
+
+    for batch_idx, (data, label) in enumerate(loader):
+        print(f"summary batch{batch_idx}")
+        data, label = data.to(device), label.to(device)
+        slide_id = slide_ids.iloc[batch_idx]
+        with torch.no_grad():
+            logits, Y_prob, Y_hat, _, _ = model(data)
+
+        acc_logger.log(Y_hat, label)
+        probs = Y_prob.cpu().numpy()
+        all_probs[batch_idx] = probs
+        all_labels[batch_idx] = label.item()
+        
+        patient_results.update({slide_id: {'slide_id': np.array(slide_id), 'prob': probs, 'label': label.item()}})
+        error = calculate_error(Y_hat, label)
+        test_error += error
+
+    test_error /= len(loader)
+
+    if n_classes == 2:
+        auc = roc_auc_score(all_labels, all_probs[:, 1])
+        aucs = []
+    else:
+        aucs = []
+        binary_labels = label_binarize(all_labels, classes=[i for i in range(n_classes)])
+        for class_idx in range(n_classes):
+            if class_idx in all_labels:
+                fpr, tpr, _ = roc_curve(binary_labels[:, class_idx], all_probs[:, class_idx])
+                aucs.append(calc_auc(fpr, tpr))
+            else:
+                aucs.append(float('nan'))
+
+        auc = np.nanmean(np.array(aucs))
+
+    return patient_results, test_error, auc, acc_logger
